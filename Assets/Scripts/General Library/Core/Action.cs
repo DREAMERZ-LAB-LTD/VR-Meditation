09fa10957f5e4e61458d95210ff0e4169cb9e576
dreamerzlabdev <dreamerzlabdev@gmail.com>
2022-05-26 13:35:56 +0600

    rename action enum

--- Assets/Scripts/General Library/Core/Action.cs
+++ Assets/Scripts/General Library/Core/Action.cs
@@ -3,32 +3,32 @@ using UnityEngine.Events;
 
 public class Action : MonoBehaviour
 {
-    [System.Flags] protected enum ExecuteAndTerminateSource
+    [System.Flags] protected enum ExecuteSource
     {
-        OnAwake = 0x00000001,
-        OnStart = 0x00000010,
-        OnEnable = 0x00000100,
-        OnDisable = 0x00001000,
-        OnDestroy = 0x00010000,
+        OnAwake     = 0x00000001,
+        OnStart     = 0x00000010,
+        OnEnable    = 0x00000100,
+        OnDisable   = 0x00001000,
+        OnDestroy   = 0x00010000,
     }
 
     [SerializeField, Tooltip("Event invoking source")]
-    protected ExecuteAndTerminateSource executeSource = 0x00000000;
+    protected ExecuteSource executeSource = 0x00000000;
 
     [Header("Calback Event"), Tooltip("Invoke it self when input source thread called")]
     [SerializeField] private UnityEvent OnExecute;
     
     
     #region Unity Thread
-    protected virtual void Awake() => ExecuteFrom(ExecuteAndTerminateSource.OnAwake);
-    protected virtual void Start() => ExecuteFrom(ExecuteAndTerminateSource.OnStart);
-    protected virtual void OnEnable() => ExecuteFrom(ExecuteAndTerminateSource.OnEnable);
-    protected virtual void OnDisable() => ExecuteFrom(ExecuteAndTerminateSource.OnDisable);
-    protected virtual void OnDestroy() => ExecuteFrom(ExecuteAndTerminateSource.OnDestroy);
+    protected virtual void Awake() => ExecuteFrom(ExecuteSource.OnAwake);
+    protected virtual void Start() => ExecuteFrom(ExecuteSource.OnStart);
+    protected virtual void OnEnable() => ExecuteFrom(ExecuteSource.OnEnable);
+    protected virtual void OnDisable() => ExecuteFrom(ExecuteSource.OnDisable);
+    protected virtual void OnDestroy() => ExecuteFrom(ExecuteSource.OnDestroy);
     #endregion Unity Thread
 
 
-    private void ExecuteFrom(ExecuteAndTerminateSource source)
+    private void ExecuteFrom(ExecuteSource source)
     {
         if ((executeSource & source) != source ||  !enabled)
             return;
