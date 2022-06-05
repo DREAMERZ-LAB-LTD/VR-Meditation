b39af648a9a6ad5201edc5fdb01e42870f8ab28d
DesignerCafe <ffahimshariar@gmail.com>
2022-06-05 12:48:09 +0600

    Particles

---
+++ Assets/AutoSave.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+/// <summary>
+/// This Script will help to save the scene change if the unity crash after hit run.
+/// </summary>
+[InitializeOnLoad]
+public class AutoSave
+{
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += SaveOnPlay;
+    }
+
+    private static void SaveOnPlay(PlayModeStateChange state)
+    {
+        if(state == PlayModeStateChange.ExitingEditMode)
+        {
+            Debug.Log("Auto-saving...");
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
