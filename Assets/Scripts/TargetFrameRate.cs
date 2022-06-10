fae173005df186ecffba1ec8b15c16465db5e036
dreamerzlabdev <dreamerzlabdev@gmail.com>
2022-06-10 21:37:52 +0600

    Update Guide, Teleport, FPS issue

--- Assets/Scripts/TargetFrameRate.cs
+++ Assets/Scripts/TargetFrameRate.cs
@@ -7,8 +7,8 @@ public class TargetFrameRate : MonoBehaviour
     [SerializeField] private int fps = 70;
     private void Awake()
     {
-        Application.targetFrameRate = fps;
-        Time.fixedDeltaTime = 1 / (float)fps;
-        Time.maximumDeltaTime = 1 / (float)fps;
+        //Application.targetFrameRate = fps;
+        //Time.fixedDeltaTime = 1 / (float)fps;
+        //Time.maximumDeltaTime = 1 / (float)fps;
     }
 }
